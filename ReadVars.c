61115657c8f7ae69656ff68659bb7adcb4fc2a6c
James Bottomley <JBottomley@Parallels.com>
2012-07-03 18:05:55 +0400

    ReadVars: Dump the extensions and save key type to file name
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- ReadVars.c
+++ ReadVars.c
@@ -11,6 +11,10 @@
 EFI_GUID GV_GUID = EFI_GLOBAL_VARIABLE;
 EFI_GUID SIG_DB = { 0xd719b2cb, 0x3d3a, 0x4596, {0xa3, 0xbc, 0xda, 0xd0,  0xe, 0x67, 0x65, 0x6f }};
 
+EFI_GUID X509_GUID =   { 0xa5c059a1, 0x94e4, 0x4aa7, {0x87, 0xb5, 0xab, 0x15, 0x5c, 0x2b, 0xf0, 0x72} };
+EFI_GUID RSA2048_GUID = { 0x3c5766e8, 0x269c, 0x4e34, {0xaa, 0x14, 0xed, 0x77, 0x6e, 0x85, 0xb3, 0xb6} };
+EFI_GUID PKCS7_GUID = { 0x4aafd29d, 0x68df, 0x49ee, {0x8a, 0xa9, 0x34, 0x7d, 0x37, 0x56, 0x65, 0xa7} };
+
 void
 parse_db(UINT8 *data, UINTN len, EFI_HANDLE image, CHAR16 *name)
 {
@@ -27,13 +31,20 @@ parse_db(UINT8 *data, UINTN len, EFI_HANDLE image, CHAR16 *name)
 		CertCount = (CertList->SignatureListSize - CertList->SignatureHeaderSize) / CertList->SignatureSize;
 		Cert      = (EFI_SIGNATURE_DATA *) ((UINT8 *) CertList + sizeof (EFI_SIGNATURE_LIST) + CertList->SignatureHeaderSize);
 
-		/* FIXME: need extensions by SignatureType */
-		ext = L"cer";
+		if (CompareGuid(&CertList->SignatureType, &X509_GUID) == 0) {
+			ext = L"X509";
+		} else if (CompareGuid(&CertList->SignatureType, &RSA2048_GUID) == 0) {
+			ext = L"RSA2048";
+		} else if (CompareGuid(&CertList->SignatureType, &PKCS7_GUID) == 0) {
+			ext = L"PKCS7";
+		} else {
+			ext = L"Unknown";
+		}
 
 		for (Index = 0; Index < CertCount; Index++) {
 			Print(L"List %d Signature %d, size %d, Type %g, GUID %g\n",
 			      count, Index, CertList->SignatureSize, &CertList->SignatureType, &Cert->SignatureOwner);
-			SPrint(buf, 0, L"%s-%d-%d-%g.%s", name, count, Index, &Cert->SignatureOwner, ext);
+			SPrint(buf, 0, L"%s-%d-%d-%s-%g", name, count, Index, ext, &Cert->SignatureOwner);
 			Print(L"Writing to file %s\n", buf);
 			status = simple_file_open(image, buf, &file, EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE);
 			if (status != EFI_SUCCESS) {

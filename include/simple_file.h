8a9a45d90419d5f6ef67c00a490b80cc9d7ec2c8
James Bottomley <JBottomley@Parallels.com>
2012-07-22 20:31:44 +0100

    simple_file: add simple directory read and filter functions
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- include/simple_file.h
+++ include/simple_file.h
@@ -8,3 +8,9 @@ void
 simple_file_close(EFI_FILE *file);
 EFI_STATUS
 generate_path(CHAR16* name, EFI_LOADED_IMAGE *li, EFI_DEVICE_PATH **grubpath, CHAR16 **PathName);
+EFI_STATUS
+simple_dir_read_all(EFI_HANDLE *image, CHAR16 *name, EFI_FILE_INFO **Entries,
+		    int *count);
+EFI_STATUS
+simple_dir_filter(EFI_HANDLE *image, CHAR16 *name, CHAR16 *filter,
+		  CHAR16 ***result, int *count, EFI_FILE_INFO **entries);
--- lib/simple_file.c
+++ lib/simple_file.c
@@ -2,6 +2,7 @@
 #include <efilib.h>
 
 #include <simple_file.h>
+#include <efiauthenticated.h>
 
 static EFI_GUID IMAGE_PROTOCOL = LOADED_IMAGE_PROTOCOL;
 static EFI_GUID SIMPLE_FS_PROTOCOL = SIMPLE_FILE_SYSTEM_PROTOCOL;
@@ -96,7 +97,7 @@ error:
 }
 
 EFI_STATUS
-simple_file_open (EFI_HANDLE image, CHAR16 *name, EFI_FILE **file, UINT64 mode)
+simple_file_open(EFI_HANDLE image, CHAR16 *name, EFI_FILE **file, UINT64 mode)
 {
 	EFI_STATUS efi_status;
 	EFI_HANDLE device;
@@ -150,6 +151,66 @@ simple_file_open (EFI_HANDLE image, CHAR16 *name, EFI_FILE **file, UINT64 mode)
 }
 
 EFI_STATUS
+simple_dir_read_all(EFI_HANDLE *image, CHAR16 *name, EFI_FILE_INFO **entries,
+		    int *count)
+{
+	EFI_FILE *file;
+	EFI_STATUS status;
+	char buf[4096];
+	UINTN size = sizeof(buf);
+	EFI_FILE_INFO *fi = (void *)buf;
+
+	status = simple_file_open(image, name, &file, EFI_FILE_MODE_READ);
+	if (status != EFI_SUCCESS) {
+		Print(L"failed to open file %s: %d\n", name, status);
+		return status;
+	}
+	
+	status = uefi_call_wrapper(file->GetInfo, 4, file, &FILE_INFO,
+				   &size, fi);
+	if (status != EFI_SUCCESS) {
+		Print(L"Failed to get file info\n");
+		goto out;
+	}
+	if ((fi->Attribute & EFI_FILE_DIRECTORY) == 0) {
+		Print(L"Not a directory %s\n", name);
+		status = EFI_INVALID_PARAMETER;
+		goto out;
+	}
+	size = 0;
+	*count = 0;
+	if (!*entries)
+		return EFI_OUT_OF_RESOURCES;
+	for (;;) {
+		int len = sizeof(buf);
+		status = uefi_call_wrapper(file->Read, 3, file, &len, buf);
+		if (status != EFI_SUCCESS || len == 0)
+			break;
+		(*count)++;
+		size += len;
+	}
+	uefi_call_wrapper(file->SetPosition, 2, file, 0);
+	Print(L"Size is %d\n", size);
+	char *ptr = AllocatePool(size);
+	*entries = (EFI_FILE_INFO *)ptr;
+	int i;
+	for (i = 0; i < *count; i++) {
+		int len = size;
+		uefi_call_wrapper(file->Read, 3, file, &len, ptr);
+		ptr += len;
+		size -= len;
+	}
+	status = EFI_SUCCESS;
+ out:
+	simple_file_close(file);
+	if (status != EFI_SUCCESS && *entries) {
+		FreePool(*entries);
+		*entries = NULL;
+	}
+	return status;
+}
+
+EFI_STATUS
 simple_file_read_all(EFI_FILE *file, UINTN *size, void **buffer)
 {
 	EFI_STATUS efi_status;
@@ -194,4 +255,60 @@ void
 simple_file_close(EFI_FILE *file)
 {
 	uefi_call_wrapper(file->Close, 1, file);
+}
+
+EFI_STATUS
+simple_dir_filter(EFI_HANDLE *image, CHAR16 *name, CHAR16 *filter,
+		  CHAR16 ***result, int *count, EFI_FILE_INFO **entries)
+{
+	EFI_STATUS status;
+	int tot, offs = StrLen(filter), i;
+	EFI_FILE_INFO *next;
+	void *ptr;
+	
+	*count = 0;
+
+	status = simple_dir_read_all(image, name, entries, &tot);
+	if (status != EFI_SUCCESS)
+		goto out;
+	ptr = next = *entries;
+
+	Print(L"%d directory entries\n", tot);
+
+	for (i = 0; i < tot; i++) {
+		int len = StrLen(next->FileName);
+
+		if (StrCmp(&next->FileName[len - offs], filter) == 0)
+			(*count) ++;
+
+		ptr += OFFSET_OF(EFI_FILE_INFO, FileName) + (len + 1)*sizeof(CHAR16);
+		next = ptr;
+	}
+	*result = AllocatePool((*count) * sizeof(void *));
+	Print(L"Got %d from filter\n", *count);
+
+	*count = 0;
+	ptr = next = *entries;
+
+	for (i = 0; i < tot; i++) {
+		int len = StrLen(next->FileName);
+
+		if (StrCmp(&next->FileName[len - offs], filter) == 0)
+			(*result)[(*count)++] = next->FileName;
+
+		ptr += OFFSET_OF(EFI_FILE_INFO, FileName) + (len + 1)*sizeof(CHAR16);
+		next = ptr;
+	}
+	status = EFI_SUCCESS;
+
+ out:
+	if (status != EFI_SUCCESS) {
+		if (*entries)
+			FreePool(*entries);
+		*entries = NULL;
+		if (*result)
+			FreePool(*result);
+		*result = NULL;
+	}
+	return status;
 }

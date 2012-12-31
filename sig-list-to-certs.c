20519c87fcd8030abb73ddee6cf229a977b2087f
James Bottomley <JBottomley@Parallels.com>
2012-12-31 13:50:55 +0000

    sig-list-to-certs: use new traversal mechanism

--- sig-list-to-certs.c
+++ sig-list-to-certs.c
@@ -7,6 +7,11 @@
 #define __STDC_VERSION__ 199901L
 #include <efi.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,72 +37,73 @@ main(int argc, char *argv[])
 	certfile = argv[2];
 	name = malloc(strlen(certfile)+10);
 
-	FILE *f = fopen(efifile, "r");
-	if (!f) {
+	int fd = open(efifile, O_RDONLY);
+	if (fd < 0) {
 		fprintf(stderr, "Failed to open file %s: ", efifile);
 		perror("");
 		exit(1);
 	}
 
-
-	int count = 0;
-
-	while (!feof(f)) {
-		EFI_SIGNATURE_LIST l;
-		EFI_SIGNATURE_DATA *d;
-		char *buf;
-
-		int s = fread(&l, 1, sizeof(l), f);
-
-		if (s == 0) {
-			/* read last cert */
-			break;
-		}
-
-		if (s != sizeof(l)) {
-			fprintf(stderr, "Only read %d bytes\n", s);
-			continue;
-		}
-
-		if (memcmp(&l.SignatureType, &EFI_CERT_X509_GUID, sizeof(EFI_GUID))==0) {
-			printf("X509 ");
-		} else if (memcmp(&l.SignatureType, &EFI_CERT_TYPE_PKCS7_GUID, sizeof(EFI_GUID))==0) {
-			printf("PKCS7 ");
-		} else if (memcmp(&l.SignatureType, &EFI_CERT_RSA2048_GUID, sizeof(EFI_GUID))==0) {
-			printf("RSA2048 ");
-		} else {
-			printf("UNKNOWN ");
-		}
-		printf("Header sls=%d, header=%d, sig=%d\n",
-		       l.SignatureListSize, l.SignatureHeaderSize, l.SignatureSize - (UINT32)OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData));
-		       
-		buf = malloc(l.SignatureListSize - sizeof(l));
-		if (!buf) {
-			fprintf(stderr, "Out of Memory\n");
-			exit(1);
-		}
-
-		s = fread(buf, 1,  l.SignatureListSize - sizeof(l), f);
-		if (s != l.SignatureListSize - sizeof(l)) {
-			fprintf(stderr, "only read %d bytes: ", s);
-			perror("");
-			exit(1);
-		}
-
-		d = (void *)buf;
-		buf += OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData);
-		EFI_GUID *guid = &d->SignatureOwner;
-
-		sprintf(name, "%s.%d",certfile,count++);
-		printf("file %s: Guid %s\n", name, guid_to_str(guid));
-		  
-
-		FILE *g = fopen(name, "w");
-		fwrite(buf, 1, l.SignatureSize - OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData), g);
-		printf("Written %d bytes\n", l.SignatureSize - (UINT32)OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData));
-		fclose(g);
+	struct stat st;
+	if (fstat(fd, &st) < 0) {
+		fprintf(stderr, "Failed to stat file %s: ", efifile);
+		perror("");
+		exit(1);
 	}
 
+	void *buf = malloc(st.st_size);
+	if (!buf) {
+		fprintf(stderr, "Malloc failed: ");
+		perror("");
+		exit(1);
+	}
+
+	if (read(fd, buf, st.st_size) != st.st_size) {
+		fprintf(stderr, "Failed to read %d bytes from %s: ",
+			(int)st.st_size, efifile);
+		perror("");
+		exit(1);
+	}
+	close(fd);
+
+	EFI_SIGNATURE_LIST *sl;
+	int s, count = 0;
+	certlist_for_each_certentry(sl, buf, s, st.st_size) {
+		EFI_SIGNATURE_DATA *sd;
+		const char *ext;
+
+		certentry_for_each_cert(sd, sl) {
+
+			if (memcmp(&sl->SignatureType, &EFI_CERT_X509_GUID, sizeof(EFI_GUID)) == 0) {
+				printf("X509 ");
+				ext = "der";
+			} else if (memcmp(&sl->SignatureType, &EFI_CERT_TYPE_PKCS7_GUID, sizeof(EFI_GUID)) == 0) {
+				printf("PKCS7 ");
+				ext = "pk7";
+			} else if (memcmp(&sl->SignatureType, &EFI_CERT_RSA2048_GUID, sizeof(EFI_GUID)) == 0) {
+				printf("RSA2048 ");
+				ext = "rsa";
+			} else if (memcmp(&sl->SignatureType, &EFI_CERT_SHA256_GUID, sizeof(EFI_GUID)) == 0) {
+				printf("SHA256 ");
+				ext = "hash";
+			} else {
+				printf("UNKNOWN ");
+				ext = "txt";
+			}
+			printf("Header sls=%d, header=%d, sig=%d\n",
+			       sl->SignatureListSize, sl->SignatureHeaderSize, sl->SignatureSize - (UINT32)OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData));
+
+			EFI_GUID *guid = &sd->SignatureOwner;
+
+			sprintf(name, "%s-%d.%s",certfile,count++,ext);
+			printf("file %s: Guid %s\n", name, guid_to_str(guid));
+
+			FILE *g = fopen(name, "w");
+			fwrite(sd->SignatureData, 1, sl->SignatureSize - OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData), g);
+			printf("Written %d bytes\n", sl->SignatureSize - (UINT32)OFFSET_OF(EFI_SIGNATURE_DATA, SignatureData));
+			fclose(g);
+		}
+	}
 
 	return 0;
 }

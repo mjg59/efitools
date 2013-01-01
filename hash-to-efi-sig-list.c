19460d7b30c5299c5b1850aa9147ff0ff8521beb
James Bottomley <JBottomley@Parallels.com>
2013-01-01 10:01:17 +0000

    hash-to-efi-sig-list: Allow creation of multiple hashes
    Within the same signature list for testing purposes.
    
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- hash-to-efi-sig-list.c
+++ hash-to-efi-sig-list.c
@@ -25,7 +25,7 @@
 static void
 usage(const char *progname)
 {
-	printf("Usage: %s efi-binary efi-signature-list\n", progname);
+	printf("Usage: %s efi-binary [efi-binary ...] efi-signature-list\n", progname);
 }
 
 static void
@@ -44,8 +44,6 @@ main(int argc, char *argv[])
 {
 	void *efifile;
 	const char *progname = argv[0];
-	struct stat st;
-	UINT8 hash[SHA256_DIGEST_SIZE];
 	int i;
 
 	while (argc > 1) {
@@ -60,42 +58,52 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if (argc != 3) {
+	if (argc < 3) {
 		usage(progname);
 		exit(1);
 	}
 
-	int fdefifile = open(argv[1], O_RDONLY);
-	if (fdefifile == -1) {
-		fprintf(stderr, "failed to open file %s: ", argv[1]);
-		perror("");
-		exit(1);
-	}
-	fstat(fdefifile, &st);
-	efifile = malloc(st.st_size);
-	read(fdefifile, efifile, st.st_size);
-	close(fdefifile);
-	sha256_get_pecoff_digest_mem(efifile, st.st_size, hash);
-	printf("HASH IS ");
-	for (i = 0; i < SHA256_DIGEST_SIZE; i++) {
-		printf("%02x", hash[i]);
-	}
-	printf("\n");
+	int hashes = argc - 2;
+	UINT8 hash[hashes][SHA256_DIGEST_SIZE];
+	
 
-	UINT8 sig[sizeof(EFI_SIGNATURE_LIST) + sizeof(EFI_SIGNATURE_DATA) - 1 + SHA256_DIGEST_SIZE];
+	for (i = 0; i < hashes; i++) {
+		int j;
+		struct stat st;
+		int fdefifile = open(argv[i + 1], O_RDONLY);
+		if (fdefifile == -1) {
+			fprintf(stderr, "failed to open file %s: ", argv[1]);
+			perror("");
+			exit(1);
+		}
+		fstat(fdefifile, &st);
+		efifile = malloc(st.st_size);
+		read(fdefifile, efifile, st.st_size);
+		close(fdefifile);
+		sha256_get_pecoff_digest_mem(efifile, st.st_size, hash[i]);
+		printf("HASH IS ");
+		for (j = 0; j < SHA256_DIGEST_SIZE; j++) {
+			printf("%02x", hash[i][j]);
+		}
+		printf("\n");
+	}
+	UINT8 sig[sizeof(EFI_SIGNATURE_LIST) + (sizeof(EFI_SIGNATURE_DATA) - 1 + SHA256_DIGEST_SIZE) * hashes];
+
 	EFI_SIGNATURE_LIST *l = (void *)sig;
-	EFI_SIGNATURE_DATA *d = (void *)sig + sizeof(EFI_SIGNATURE_LIST);
 
 	memset(sig, 0, sizeof(sig));
 	l->SignatureType = EFI_CERT_SHA256_GUID;
 	l->SignatureListSize = sizeof(sig);
 	l->SignatureSize = 16 +32; /* UEFI defined */
-	d->SignatureOwner = MOK_OWNER;
-	memcpy(&d->SignatureData, hash, sizeof(hash));
+	for (i = 0; i < hashes; i++) {
+		EFI_SIGNATURE_DATA *d = (void *)sig + sizeof(EFI_SIGNATURE_LIST) + l->SignatureSize * i;
+		d->SignatureOwner = MOK_OWNER;
+		memcpy(&d->SignatureData, hash[i], sizeof(hash[i]));
+	}
 
-	int fdoutfile = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, S_IWUSR|S_IRUSR);
+	int fdoutfile = open(argv[hashes + 1], O_CREAT|O_WRONLY|O_TRUNC, S_IWUSR|S_IRUSR);
 	if (fdoutfile == -1) {
-		fprintf(stderr, "failed to open %s: ", argv[2]);
+		fprintf(stderr, "failed to open %s: ", argv[hashes + 1]);
 		perror("");
 		exit(1);
 	}

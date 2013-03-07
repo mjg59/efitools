0980748056e5af08f551f779e0755a783325953f
James Bottomley <JBottomley@Parallels.com>
2013-03-07 13:01:05 +0000

    cert-to-efi-sig-list: remove unimplemented RSA2048 certificate support

--- cert-to-efi-sig-list.c
+++ cert-to-efi-sig-list.c
@@ -23,18 +23,16 @@
 static void
 usage(const char *progname)
 {
-	printf("Usage: %s [-g <guid>] [-r] <crt file> <efi sig list file>\n", progname);
+	printf("Usage: %s [-g <guid>] <crt file> <efi sig list file>\n", progname);
 }
 
 static void
 help(const char * progname)
 {
 	usage(progname);
-	printf("Take an input certificate (in PEM format) and convert it to an EFI\n"
+	printf("Take an input X509 certificate (in PEM format) and convert it to an EFI\n"
 	       "signature list file containing only that single certificate\n\n"
 	       "Options:\n"
-	       "\t-r               The file contains an RSA2048 certificate instead of the\n"
-	       "\t                 Default X509 format [UNIMPLEMENTED]\n"
 	       "\t-g <guid>        Use <guid> as the owner of the signature. If this is not\n"
 	       "\t                 supplied, an all zero guid will be used\n"
 
@@ -47,7 +45,6 @@ main(int argc, char *argv[])
 {
 	char *certfile, *efifile;
 	const char *progname = argv[0];
-	int rsasig;
 	EFI_GUID owner = { 0 };
 
 	while (argc > 1) {
@@ -61,10 +58,6 @@ main(int argc, char *argv[])
 			str_to_guid(argv[2], &owner);
 			argv += 2;
 			argc -= 2;
-		} else if (strcmp("-r", argv[1]) == 0) {
-			rsasig = 1;
-			argv += 1;
-			argc += 1;
 		} else {
 			break;
 		}
